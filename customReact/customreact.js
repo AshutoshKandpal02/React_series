6965f3f9137edfa3aae23ad4f5c0de6b793e0447
Ashutosh Kandpal <ashutoshkanpal240@gmail.com>
2026-01-20 22:40:58 +0530

    React Revision

---
+++ customReact/customreact.js
@@ -0,0 +1,28 @@
+function customRender(reactElement, container) {
+    /*
+    const domElement=document.createElement(reactElement.type)
+    domElement.innerHTML=reactElement.children
+    domElement.setAttribute('href',reactElement.props.href)
+    domElement.setAttribute('target',reactElement.props.target)
+
+    container.appendChild(domElement)
+    */
+    const domElement = document.createElement(reactElement.type)
+    domElement.innerHTML = reactElement.children
+    for (const prop in reactElement.props) {
+        if (prop === 'children') continue; //if children is inside the props then this condition
+        domElement.setAttribute(prop, reactElement.props[prop])
+    }
+    container.appendChild(domElement)
+}
+const reactElement = {
+    type: 'a',
+    props: {   //props is a object in which properties can be added 
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+const mainContainer = document.getElementById('root');
+
+customRender(reactElement, mainContainer)
