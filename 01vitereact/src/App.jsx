880502e717342f959965c665c3ff4172b019f5b7
Ashutosh Kandpal <ashutoshkanpal240@gmail.com>
2026-01-20 23:29:22 +0530

    Own React Library And Evaluated Expression

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,8 +1,12 @@
 
 function App() {
+  const username="Ashutosh"
   return (
-    <h1>Hello Vite React</h1>
+    <h1>Hello Vite React {username}</h1>     //When there is curly braces then iit is treated as variable 
   )
 }
 
 export default App
+
+//{username} is evaluated expression means the final or evaluated outcome of the javascript 
+//because as it is converted into object so we can't  evaluate a object so it evaluated synatx
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
+import React from 'react' 
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return(
+    <h1>Hello React!</h1>
+  )
+}
+
+// const ReactElement = {
+//     type: 'a',
+//     props: {   //props is a object in which properties can be added 
+//         href: 'https://google.com',
+//         target: '_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const anotheruser="chai"
+const reactElement= React.createElement(
+  'a',
+  {href:'https://google.com' ,target:'_blank'},
+  'Click me to visit google ',
+  anotheruser
 )
+
+createRoot(document.getElementById('root')).render(
+  reactElement
+);
+
+
+
+//<MyApp/>  As it is function so we can also write it as MyApp() but we have to follow the conventions
+//so write "ReactElement" as it is object and object can be dirently called
+//a function is converted into elements first then it is rendered and it is done by react itself inside it
